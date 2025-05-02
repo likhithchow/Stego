261d29afae9555c9ab9e54ae44135ad07e66039a
Choudari Likhith <choudarilikhith@gmail.com>
2025-05-03 03:30:09 +0530

    old verion 7

--- stegano_app/views.py
+++ stegano_app/views.py
@@ -221,31 +221,39 @@ def encryption_view(request):
 def decryption_view(request):
     global shared_image, shared_text
     text = ''
+    warning = ''
 
     if request.method == 'POST':
         image_file = request.FILES.get('image')
 
         if not image_file:
-            return render(request, 'decryption.html', {'text': 'Please upload an image.'})
+            warning = "Please upload an image."
+            return render(request, 'decryption.html', {'text': '', 'warning': warning})
 
         try:
             image = Image.open(image_file)
 
-            # Reject SVG
             if image.format == 'SVG':
-                return render(request, 'decryption.html', {'text': 'SVG format is not supported for decryption.'})
+                warning = "SVG format is not supported for decryption."
+                return render(request, 'decryption.html', {'text': '', 'warning': warning})
 
             if image.mode != 'RGBA':
                 image = image.convert('RGBA')
 
-            text = extract_text_from_image(image)
+            decoded = extract_text_from_image(image)
+            if not decoded.startswith("MSG:"):
+                warning = "No valid hidden message found in this image."
+                return render(request, 'decryption.html', {'text': '', 'warning': warning})
+
+            text = decoded[4:]  # Remove prefix
             shared_image = image.copy()
             shared_text = text
 
         except UnidentifiedImageError:
-            return render(request, 'decryption.html', {'text': 'Unsupported or corrupted image format.'})
+            warning = "Unsupported or corrupted image format."
+            return render(request, 'decryption.html', {'text': '', 'warning': warning})
 
-    return render(request, 'decryption.html', {'text': text})
+    return render(request, 'decryption.html', {'text': text, 'warning': warning})
 
 def dashboard_view(request):
     global shared_image, shared_text
